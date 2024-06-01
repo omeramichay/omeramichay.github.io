2305358fe4a57b3a394718f1322845a44efaaa50
omeramichay <93185395+omeramichay@users.noreply.github.com>
2024-06-01 21:45:08 +0300

    newtime.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-const countdownDate = new Date("Jun 21, 2024 00:00:00").getTime();
+const countdownDate = new Date("Jun 8, 2024 03:41:22").getTime();
 
 // Update the countdown every 1 second
 const countdownFunction = setInterval(function() {
